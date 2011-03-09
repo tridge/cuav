e90542b79a41be9c080f236eeb628e802ae920f4
Andrew Tridgell <tridge@samba.org>
2011-03-09 19:06:49 +1100

    improved capture code

--- camera/capture_images.c
+++ camera/capture_images.c
@@ -34,8 +34,11 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <string.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <arpa/inet.h>
+#include <math.h>
+#include <getopt.h>
 
 #define SHUTTER_63US    9
 #define SHUTTER_1MS    24
@@ -43,42 +46,43 @@
 #define SHUTTER_20MS  105
 #define SHUTTER_100MS 264
 
-#define SHUTTER_MIN     8
-#define SHUTTER_MAX   264
-#define SHUTTER_GOOD   20
+#define SHUTTER_MIN     0.000010
+#define SHUTTER_MAX     0.010000
+#define SHUTTER_GOOD    0.001000
 
-#define GAIN_2dB      217
-#define GAIN_5dB      303
-#define GAIN_10dB     445
+#define GAIN_MIN      0.0
+#define GAIN_MAX      20.0
+#define GAIN_GOOD     1
 
-#define GAIN_MIN      160
-#define GAIN_MAX      445
-#define GAIN_GOOD     189
+#define AVERAGE_LOW    7500
+#define AVERAGE_HIGH   8500
+#define AVERAGE_TARGET 8000
 
-#define AVERAGE_LOW   7000
-#define AVERAGE_HIGH  12000
-
-#define SATURATED_HIGH 1000
-
-#define SHUTTER_FACTOR 1.2
-#define GAIN_FACTOR    1.1
+#define SATURATED_HIGH 100
+#define SATURATION_THRESHOLD 60000
 
 
 #define IMAGE_WIDTH 1280
 #define IMAGE_HEIGHT 960
 
-static void get_averages(uint16_t *image, uint16_t *average, uint32_t *num_saturated)
+static void get_averages(uint16_t *image, uint16_t *average, 
+			 uint32_t *num_saturated, uint32_t *num_half_saturated)
 {
 	double total = 0;
 	int i;
 	uint16_t highest=0;
 
 	*num_saturated = 0;
+	*num_half_saturated = 0;
+
 	for (i=0; i<IMAGE_WIDTH*IMAGE_HEIGHT; i++) {
 		uint16_t v = ntohs(image[i]);
 		total += v;
-		if (v > 65500) {
+		if (v > SATURATION_THRESHOLD) {
 			(*num_saturated)++;
+		}
+		if (v > SATURATION_THRESHOLD/2) {
+			(*num_half_saturated)++;
 		}
 		if (v > highest) {
 			highest = v;
@@ -89,18 +93,80 @@ static void get_averages(uint16_t *image, uint16_t *average, uint32_t *num_satur
 
 #define CHECK(x) do { if ((x) != DC1394_SUCCESS) return -1; } while (0)
 
+static float new_gain(float current_average, float target_average, float current_gain)
+{
+	float decibel_change = 10.0 * log10(target_average/current_average);
+	float gain;
+	gain = current_gain + (0.3*decibel_change);
+	if (gain < GAIN_MIN) gain = GAIN_MIN;
+	if (gain > GAIN_MAX) gain = GAIN_MAX;
+	return gain;
+}
+
+static float new_shutter(float current_average, float target_average, float current_shutter, 
+			 float shutter_max)
+{
+	float shutter = current_shutter * (target_average/current_average);
+	if (shutter > shutter_max) shutter = shutter_max;
+	if (shutter < SHUTTER_MIN) shutter = SHUTTER_MIN;
+	return (0.7*current_shutter)+(0.3*shutter);
+}
+
 static int capture_image(dc1394camera_t *camera, const char *fname,
-			 uint16_t *shutter, uint16_t *gain,
-			 uint16_t *average, uint32_t *num_saturated)
+			 float shutter, float gain,
+			 uint16_t *average, 
+			 uint32_t *num_saturated, 
+			 uint32_t *num_half_saturated, 
+			 bool testonly)
 {
 	int fd;
 	dc1394video_frame_t *frame;
 
-	CHECK(dc1394_camera_reset(camera));
+	CHECK(dc1394_feature_set_absolute_value(camera, DC1394_FEATURE_GAIN, gain));
+	CHECK(dc1394_feature_set_absolute_value(camera, DC1394_FEATURE_SHUTTER, shutter));
+	CHECK(dc1394_video_set_one_shot(camera, DC1394_ON));
+	CHECK(dc1394_capture_dequeue(camera, DC1394_CAPTURE_POLICY_WAIT, &frame));
+
+	get_averages((uint16_t *)frame->image, average, num_saturated, num_half_saturated);
+
+	if (*average == 0) {
+		/* bad frame */
+		return -1;
+	}
+
+	if (!testonly) {
+		fd = open(fname, O_WRONLY|O_CREAT|O_TRUNC, 0644);
+		if (fd == -1) {
+			fprintf(stderr, "Can't create imagefile '%s' - %s", fname, strerror(errno));
+			return -1;
+		}
+		
+		dprintf(fd,"P5\n%u %u\n#PARAM: t=%llu shutter=%f gain=%f average=%u saturated=%u\n65535\n", 
+			IMAGE_WIDTH, IMAGE_HEIGHT, (unsigned long long)frame->timestamp, shutter, gain,
+			*average, *num_saturated);
+		if (write(fd, frame->image, IMAGE_WIDTH*IMAGE_HEIGHT*2) != IMAGE_WIDTH*IMAGE_HEIGHT*2) {
+			fprintf(stderr, "Write failed for %s\n", fname);
+		}
+		close(fd);
+	}
+
+	CHECK(dc1394_capture_enqueue(camera,frame));
+
+	return 0;
+}
+
+static int capture_loop(dc1394camera_t *camera, const char *basename, float delay, bool testonly)
+{
+	uint16_t average;
+	uint32_t num_saturated, num_half_saturated;
+	float shutter = SHUTTER_GOOD;
+	float gain = GAIN_GOOD;
 
 	CHECK(dc1394_video_set_iso_speed(camera, DC1394_ISO_SPEED_400));
 	CHECK(dc1394_video_set_mode(camera, DC1394_VIDEO_MODE_1280x960_MONO16));
 	CHECK(dc1394_video_set_framerate(camera, DC1394_FRAMERATE_7_5));
+
+	CHECK(dc1394_capture_setup(camera, 4, DC1394_CAPTURE_FLAGS_DEFAULT));
 
 	CHECK(dc1394_feature_set_power(camera, DC1394_FEATURE_EXPOSURE, DC1394_OFF));
 
@@ -108,69 +174,17 @@ static int capture_image(dc1394camera_t *camera, const char *fname,
 	CHECK(dc1394_feature_set_mode(camera, DC1394_FEATURE_BRIGHTNESS, DC1394_FEATURE_MODE_MANUAL));
 	CHECK(dc1394_feature_set_value(camera, DC1394_FEATURE_BRIGHTNESS, 0));
 
-	if (gain == NULL) {
-		CHECK(dc1394_feature_set_power(camera, DC1394_FEATURE_GAIN, DC1394_ON));
-		CHECK(dc1394_feature_set_mode(camera, DC1394_FEATURE_GAIN, DC1394_FEATURE_MODE_AUTO));
-	} else {
-		uint32_t min_gain, max_gain;
-		CHECK(dc1394_feature_set_power(camera, DC1394_FEATURE_GAIN, DC1394_ON));
-		CHECK(dc1394_feature_set_mode(camera, DC1394_FEATURE_GAIN, DC1394_FEATURE_MODE_MANUAL));
-		CHECK(dc1394_feature_get_boundaries(camera, DC1394_FEATURE_GAIN, &min_gain, &max_gain));
-		if (*gain < min_gain) *gain = min_gain;
-		if (*gain > max_gain) *gain = max_gain;
-		CHECK(dc1394_feature_set_value(camera, DC1394_FEATURE_GAIN, *gain));
-	}
+	CHECK(dc1394_feature_set_power(camera, DC1394_FEATURE_GAIN, DC1394_ON));
+	CHECK(dc1394_feature_set_mode(camera, DC1394_FEATURE_GAIN, DC1394_FEATURE_MODE_MANUAL));
+	CHECK(dc1394_feature_set_value(camera, DC1394_FEATURE_GAIN, 500));
+	CHECK(dc1394_feature_set_absolute_control(camera, DC1394_FEATURE_GAIN, DC1394_ON));
+	CHECK(dc1394_feature_set_absolute_value(camera, DC1394_FEATURE_GAIN, GAIN_GOOD));
 
-	if (shutter == NULL) {
-		CHECK(dc1394_feature_set_power(camera, DC1394_FEATURE_SHUTTER, DC1394_ON));
-		CHECK(dc1394_feature_set_mode(camera, DC1394_FEATURE_SHUTTER, DC1394_FEATURE_MODE_AUTO));
-	} else {
-		uint32_t min_shutter, max_shutter;
-		CHECK(dc1394_feature_set_power(camera, DC1394_FEATURE_SHUTTER, DC1394_ON));
-		CHECK(dc1394_feature_set_mode(camera, DC1394_FEATURE_SHUTTER, DC1394_FEATURE_MODE_MANUAL));
-		CHECK(dc1394_feature_get_boundaries(camera, DC1394_FEATURE_SHUTTER, &min_shutter, &max_shutter));
-		if (*shutter < min_shutter) *shutter = min_shutter;
-		if (*shutter > max_shutter) *shutter = max_shutter;
-		CHECK(dc1394_feature_set_value(camera, DC1394_FEATURE_SHUTTER, *shutter));
-	}
-
-
-	CHECK(dc1394_capture_setup(camera, 1, DC1394_CAPTURE_FLAGS_DEFAULT));
-
-	CHECK(dc1394_video_set_transmission(camera, DC1394_ON));
-
-	CHECK(dc1394_capture_dequeue(camera, DC1394_CAPTURE_POLICY_WAIT, &frame));
-
-	get_averages((uint16_t *)frame->image, average, num_saturated);
-
-	fd = open(fname, O_WRONLY|O_CREAT|O_TRUNC, 0644);
-	if (fd == -1) {
-		fprintf(stderr, "Can't create imagefile '%s' - %s", fname, strerror(errno));
-		return -1;
-	}
-
-	dprintf(fd,"P5\n%u %u\n#PARAM: t=%llu shutter=%u gain=%u average=%u saturated=%u\n65535\n", 
-		IMAGE_WIDTH, IMAGE_HEIGHT, (unsigned long long)frame->timestamp, shutter?*shutter:0, gain?*gain:0,
-		*average, *num_saturated);
-	if (write(fd, frame->image, IMAGE_WIDTH*IMAGE_HEIGHT*2) != IMAGE_WIDTH*IMAGE_HEIGHT*2) {
-		fprintf(stderr, "Write failed for %s\n", fname);
-	}
-
-	CHECK(dc1394_capture_enqueue(camera,frame));
-
-	CHECK(dc1394_video_set_transmission(camera,DC1394_OFF));
-	CHECK(dc1394_capture_stop(camera));
-
-	close(fd);
-	return 0;
-}
-
-static int capture_loop(dc1394camera_t *camera, const char *basename, float delay)
-{
-	uint16_t average;
-	uint32_t num_saturated;
-	uint16_t shutter = SHUTTER_GOOD;
-	uint16_t gain = GAIN_GOOD;
+	CHECK(dc1394_feature_set_power(camera, DC1394_FEATURE_SHUTTER, DC1394_ON));
+	CHECK(dc1394_feature_set_mode(camera, DC1394_FEATURE_SHUTTER, DC1394_FEATURE_MODE_MANUAL));
+	CHECK(dc1394_feature_set_value(camera, DC1394_FEATURE_SHUTTER, 500));
+	CHECK(dc1394_feature_set_absolute_control(camera, DC1394_FEATURE_SHUTTER, DC1394_ON));
+	CHECK(dc1394_feature_set_absolute_value(camera, DC1394_FEATURE_SHUTTER, SHUTTER_GOOD));
 
 	while (true) {
 		char *fname;
@@ -185,46 +199,54 @@ static int capture_loop(dc1394camera_t *camera, const char *basename, float dela
 
 		strftime(tstring, sizeof(tstring), "%Y%m%d%H%M%S", tm);
 
-		if (asprintf(&fname, "%s-%s-%02u-auto.pgm", 
-			     basename, tstring, (unsigned)(tv.tv_usec/10000)) == -1) {
-			return -1;
-		}
-		if (capture_image(camera, fname, NULL, NULL, &average, &num_saturated) == -1) {
-			return -1;
-		}
-		free(fname);
-
-
 		if (asprintf(&fname, "%s-%s-%02u.pgm", 
 			     basename, tstring, (unsigned)(tv.tv_usec/10000)) == -1) {
 			return -1;
 		}
-		if (capture_image(camera, fname, &shutter, &gain, &average, &num_saturated) == -1) {
+		if (capture_image(camera, fname, shutter, gain, &average, 
+				  &num_saturated, &num_half_saturated, testonly) == -1) {
 			return -1;
 		}
-		printf("%s shutter=%u gain=%u average=%u num_saturated=%u\n", 
-		       fname, shutter, gain, average, num_saturated);
+		printf("%s shutter=%f gain=%f average=%u saturated=%u hsaturated=%u\n", 
+		       fname, shutter, gain, average, num_saturated, num_half_saturated);
 		free(fname);
 
-		if (average < AVERAGE_LOW) {
+		if (num_saturated > SATURATED_HIGH) {
+			/* too much saturation */
+			if (gain > GAIN_MIN) {
+				gain = new_gain(average, average*0.5, gain);
+			} else if (shutter > SHUTTER_MIN) {
+				shutter = new_shutter(average, average*0.5, shutter, SHUTTER_MAX);
+			}
+		} else if (average < AVERAGE_LOW && 
+			   num_saturated == 0 && 
+			   num_half_saturated < SATURATED_HIGH) {
 			/* too dark */
 			if (shutter < SHUTTER_GOOD) {
-				shutter *= SHUTTER_FACTOR;
-			} else if (gain < GAIN_MAX) {
-				gain *= GAIN_FACTOR;
-			} else if (shutter < SHUTTER_MAX) {
-				shutter *= SHUTTER_FACTOR;
+				float shutter2 = new_shutter(average, AVERAGE_TARGET, shutter, SHUTTER_GOOD);
+				average = (shutter2/shutter)*average;
+				shutter = shutter2;
 			}
-		}
-		if (average > AVERAGE_HIGH ||
-		    num_saturated > SATURATED_HIGH) {
+			if (average < AVERAGE_LOW) {
+				if (gain < GAIN_MAX) {
+					gain = new_gain(average, AVERAGE_TARGET, gain);
+				} else if (shutter < SHUTTER_MAX) {
+					shutter = new_shutter(average, AVERAGE_TARGET, shutter, SHUTTER_MAX);
+				}
+			}
+		} else if (average > AVERAGE_HIGH) {
 			/* too light */
 			if (shutter > SHUTTER_GOOD) {
-				shutter /= SHUTTER_FACTOR;
-			} else if (gain > GAIN_MIN) {
-				gain /= GAIN_FACTOR;
-			} else if (shutter > SHUTTER_MIN) {
-				shutter /= SHUTTER_FACTOR;
+				float shutter2 = new_shutter(average, AVERAGE_TARGET, shutter, SHUTTER_GOOD);
+				average = (shutter2/shutter)*average;
+				shutter = shutter2;
+			}
+			if (average > AVERAGE_HIGH) {
+				if (gain > GAIN_MIN) {
+					gain = new_gain(average, AVERAGE_TARGET, gain);
+				} else if (shutter > SHUTTER_MIN) {
+					shutter = new_shutter(average, AVERAGE_TARGET, shutter, SHUTTER_MAX);
+				}
 			}
 		}
 
@@ -273,13 +295,12 @@ static dc1394camera_t *open_camera(void)
 
 static void close_camera(dc1394camera_t *camera)
 {
-	dc1394_video_set_transmission(camera, DC1394_OFF);
 	dc1394_capture_stop(camera);
 	dc1394_camera_free(camera);
 }
 
 
-static int run_capture(void)
+static int run_capture(const char *basename, float delay, bool testonly)
 {
 	dc1394camera_t *camera;
 
@@ -287,18 +308,54 @@ static int run_capture(void)
 		sleep(1);
 	}
 
-	capture_loop(camera, "test", 0.01);
+	capture_loop(camera, basename, delay, testonly);
 	close_camera(camera);
 
 	return 0;
 }
 
+static void usage(void)
+{
+	printf("capture_images [options]\n");
+	printf("\t-d delay       delay between images (seconds)\n");
+	printf("\t-b basename    base filename\n");
+}
 
 int main(int argc, char *argv[])
 {
+	const char *basename = "test";
+	float delay = 0.0;
+	bool testonly = false;
+	int opt;
+
+	while ((opt = getopt(argc, argv, "d:b:th")) != -1) {
+		switch (opt) {
+		case 'd':
+			delay = atof(optarg);
+			break;
+		case 'b':
+			basename = optarg;
+			break;
+		case 't':
+			testonly = true;
+			break;
+		case 'h':
+			usage();
+			exit(0);
+			break;
+		default:
+			printf("Invalid option '%c'\n", opt);
+			usage();
+			exit(1);
+		}
+	}
+
+	argv += optind;
+	argc -= optind;
+
 	while (true) {
 		printf("Starting capture\n");
-		run_capture();
+		run_capture(basename, delay, testonly);
 	}
 	return 0;
 }
