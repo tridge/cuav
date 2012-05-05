543c235f6ca58eb8d35cbf529ca3338fef07ecb1
Andrew Tridgell <tridge@samba.org>
2012-05-05 20:04:01 +1000

    fixed py_capture for new API

--- camera/py_capture.py
+++ camera/py_capture.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import chameleon, numpy
+import chameleon, numpy, os
 
 colour = 1
 depth = 8
@@ -8,11 +8,16 @@ h = chameleon.open(colour, depth)
 
 im = numpy.zeros((960,1280),dtype='uint8')
 
+try:
+  os.mkdir('tmp')
+except os.error:
+  pass
+
 for i in range(0,10):
   try:
     chameleon.trigger(h)
-    (shutter, ftime) = chameleon.capture(h, im)
-    filename = 'i%u.pgm' % i
+    shutter = chameleon.capture(h, im)
+    filename = 'tmp/i%u.pgm' % i
     chameleon.save_pgm(h, filename, im)
     print("Captured to %s" % filename)
   except chameleon.error:
