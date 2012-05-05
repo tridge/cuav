e4a87bdfb3209efa51074db2958c99863e1e6bc2
Andrew Tridgell <tridge@samba.org>
2012-05-05 20:02:36 +1000

    fixed py_scanner for new API

--- camera/py_scanner.py
+++ camera/py_scanner.py
@@ -17,38 +17,45 @@ except chameleon.error:
 print("Found camera: colour=%u GUID=%x" % (colour, chameleon.guid(h)))
 im = numpy.zeros((960,1280),dtype='uint8')
 im_640 = numpy.zeros((480,640,3),dtype='uint8')
-im_marked = numpy.zeros((480,640,3),dtype='uint8')
 
 cv.NamedWindow('Viewer')
 
 tstart = time.time()
 
+try:
+  os.mkdir('tmp')
+except os.error:
+  pass
+
 i=0
 while True:
   try:
     chameleon.trigger(h)
-    (shutter, ftime) = chameleon.capture(h, im)
+    frame_time = time.time()
+    shutter = chameleon.capture(h, im)
   except chameleon.error, msg:
     print('failed to capture', msg)
     continue
   scanner.debayer(im, im_640)
-  regions = scanner.scan(im_640, im_marked)
+  regions = scanner.scan(im_640)
   if len(regions) > 0:
     print("Found %u regions" % len(regions))
     for r in regions:
       (minx, miny, maxx, maxy) = r
       print(minx, miny, maxx, maxy)
 
-  mat = cv.fromarray(im_marked)
-  img = cv.GetImage(mat)
-
+  mat = cv.fromarray(im_640)
+  for (x1,y1,x2,y2) in regions:
+    cv.Rectangle(mat, (x1,y1), (x2,y2), (255,0,0), 1)
+  im_marked = numpy.ascontiguousarray(mat)
+  
   # compress using neon-accelerated compressor, and write to a file
   jpeg = scanner.jpeg_compress(im_marked)
   jfile = open('tmp/i%u.jpg' % i, "w")
   jfile.write(jpeg)
   jfile.close()
 
-  cv.ShowImage('Viewer', img)
+  cv.ShowImage('Viewer', im_marked)
   i += 1
 
   if i % 10 == 0:
