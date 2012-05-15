7d28e0b061a99ebaf9e4d3f89c2c9a5387835087
Andrew Tridgell <tridge@samba.org>
2012-05-15 16:21:56 +1000

    fixed byte order in save_pgm()

--- camera/chameleon_py.c
+++ camera/chameleon_py.c
@@ -208,12 +208,22 @@ static int _save_pgm(const char *filename, unsigned w, unsigned h, unsigned stri
 		     const char *data)
 {
 	int fd = open(filename, O_WRONLY|O_CREAT|O_TRUNC, 0644);
+	int ret;
+
 	if (fd == -1) {
 		return -1;
 	}
 	dprintf(fd,"P5\n%u %u\n%u\n", 
 		w, h, stride==w?255:65535);
-	if (write(fd, data, h*stride) != h*stride) {
+	if (__BYTE_ORDER == __LITTLE_ENDIAN && stride == w*2) {
+		char *data2 = malloc(w*h*2);
+		swab(data, data2, w*h*2);
+		ret = write(fd, data2, h*stride);
+		free(data2);
+	} else {
+		ret = write(fd, data, h*stride);
+	}
+	if (ret != h*stride) {
 		close(fd);
 		return -1;
 	}
