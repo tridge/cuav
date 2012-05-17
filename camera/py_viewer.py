dd0e3369f70f4c4364568c18cd736e812d18a8d4
Andrew Tridgell <tridge@samba.org>
2012-05-17 16:08:09 +1000

    added gamma and brightness to py_viewer

--- camera/py_viewer.py
+++ camera/py_viewer.py
@@ -2,20 +2,29 @@
 
 import chameleon, cv, time, sys, os, numpy
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'image'))
-import scanner
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'lib'))
+import scanner, cuav_util
+
+from optparse import OptionParser
+parser = OptionParser("py_viewer.py [options]")
+parser.add_option("--depth", type='int', default=8, help="capture depth")
+parser.add_option("--output", default='tmp', help="output directory")
+parser.add_option("--half", action='store_true', default=False, help="show half res")
+parser.add_option("--brightness", type='float', default=1.0, help="display brightness")
+parser.add_option("--gamma", type='int', default=1024, help="set gamma (if 16 bit images)")
+(opts, args) = parser.parse_args()
 
 
 colour = 0
-depth = 16
 try:
-  h = chameleon.open(1, depth, 100)
+  h = chameleon.open(1, opts.depth, 100)
   colour = 1
 except chameleon.error:
-  h = chameleon.open(0, depth, 100)
+  h = chameleon.open(0, opts.depth, 100)
   colour = 0
 
 print("Found camera: colour=%u GUID=%x" % (colour, chameleon.guid(h)))
-if depth == 8:
+if opts.depth == 8:
   dtype = 'uint8'
 else:
   dtype = 'uint16'
@@ -26,31 +35,38 @@ cv.NamedWindow('Viewer')
 tstart = time.time()
 
 chameleon.trigger(h, True)
+chameleon.set_gamma(h, opts.gamma)
+
+cuav_util.mkdir_p(opts.output)
 
 i=0
+lost = 0
 while True:
   try:
-    frame_time, frame_counter, shutter = chameleon.capture(h, 1000, im)
+    frame_time, frame_counter, shutter = chameleon.capture(h, 300, im)
   except chameleon.error, msg:
-    print('failed to capture', msg)
+    lost += 1
     continue
-  filename = 'tmp/i%u.pgm' % i
+  filename = '%s/i%u.pgm' % (opts.output, i)
   chameleon.save_pgm(filename, im)
-  if colour == 1:
-    img_colour = numpy.zeros((480,640,3),dtype='uint8')
-    scanner.debayer(im, img_colour)
-    img_640 = cv.GetImage(cv.fromarray(img_colour))
-  else:
-    img_640 = cv.CreateImage((640,480), 8, 1)
-    mat = cv.fromarray(im)
-    img = cv.GetImage(mat)
-    cv.Resize(img, img_640)
-  cv.ShowImage('Viewer', img_640)
+  img_colour = numpy.zeros((960,1280,3),dtype='uint8')
+  scanner.debayer_full(im, img_colour)
+  img_colour = cv.GetImage(cv.fromarray(img_colour))
+  if opts.half:
+    img_640 = cv.CreateImage((640,480), 8, 3)
+    cv.Resize(img_colour, img_640)
+    img_colour = img_640
+
+  cv.ConvertScale(img_colour, img_colour, scale=opts.brightness)
+  cv.ShowImage('Viewer', img_colour)
+  key = cv.WaitKey(1)
   i += 1
 
   if i % 10 == 0:
     tdiff = time.time() - tstart
-    print("%.1f fps shutter=%f" % (10/tdiff, shutter));
+    print("captured %u  lost %u  %.1f fps shutter=%f" % (
+      i, lost,
+      10/tdiff, shutter));
     tstart = time.time()
 
   key = cv.WaitKey(1)
