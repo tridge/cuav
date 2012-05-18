8b59de0e38dea9aecf188043f2eff8d2c9523c29
Andrew Tridgell <tridge@samba.org>
2012-05-18 22:42:27 +1000

    added joe iterator

--- lib/cuav_joe.py
+++ lib/cuav_joe.py
@@ -6,7 +6,7 @@ Andrew Tridgell
 May 2012
 '''
 
-import os, sys, cuav_util, mav_position, cPickle
+import os, sys, cuav_util, mav_position, cPickle, time
 
 
 class JoePosition():
@@ -18,6 +18,10 @@ class JoePosition():
       self.pos = pos
       self.image_filename = image_filename
 
+  def __str__(self):
+    return 'JoePosition(lat=%f lon=%f %s %s %s)' % (self.latlon[0], self.latlon[1],
+                                                    self.pos, self.image_filename,
+                                                    time.asctime(time.localtime(self.frame_time)))
       
 class JoeLog():
     '''a Joe position logger'''
@@ -37,3 +41,24 @@ class JoeLog():
             (lat, lon) = cuav_util.gps_position_from_image_region(r, pos)
             self.add((lat,lon), frame_time, pos, image_filename)
             
+class JoeIterator():
+  '''an iterator for a joe.log'''
+  def __init__(self, filename):
+    self.log = open(filename, "r")
+
+  def __iter__(self):
+    return self
+
+  def next(self):
+    try:
+      joe = cPickle.load(self.log)
+    except Exception:
+      raise StopIteration
+    return joe
+  
+
+if __name__ == "__main__":
+  joelog = JoeIterator(sys.argv[1])
+  for joe in joelog:
+    print joe
+    
