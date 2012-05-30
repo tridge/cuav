679cc39d6fa7ba5f78445888e727caf2db46b93c
Andrew Tridgell <tridge@samba.org>
2012-05-30 22:25:08 +1000

    playback: respond to parameter fetch requests

--- tests/playback.py
+++ tests/playback.py
@@ -67,10 +67,15 @@ def playback(filename, images):
     while len(images) and images[0].frame_time < msg._timestamp:
         images.pop(0)
 
+    params = []
+    param_send = []
+
     while True:
         msg = mlog.recv_match(condition=opts.condition)
         if msg is None:
             return
+        if msg.get_type() == 'PARAM_VALUE':
+            params.append(msg)
         for m in mout:
             m.write(msg.get_msgbuf())
         deltat = msg._timestamp - last_timestamp
@@ -82,6 +87,7 @@ def playback(filename, images):
         if time.time() - last_print > 2.0:
             print('%s' % (time.asctime(time.localtime(msg._timestamp))))
             last_print = time.time()
+
         if len(images) and msg._timestamp > images[0].frame_time:
             img = images.pop(0)
             try:
@@ -91,6 +97,20 @@ def playback(filename, images):
             os.symlink(img.filename, 'fake_chameleon.tmp')
             os.rename('fake_chameleon.tmp', 'fake_chameleon.pgm')
             print(img.filename)
+
+        # check for parameter fetch messages
+        for m in mout:
+            msg = m.recv_msg()
+            if msg and msg.get_type() == 'PARAM_REQUEST_LIST':
+                print("Sending %u parameters" % len(params))
+                param_send = params[:]
+
+        if len(param_send) != 0:
+            for m in mout:
+                p = param_send.pop(0)
+                m.write(p.get_msgbuf())
+
+
 
 images = scan_image_directory(opts.imagedir)
 if len(images) == 0:
