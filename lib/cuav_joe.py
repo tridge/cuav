f56eef5c7e92d9890fa18c58b00837467991219f
Andrew Tridgell <tridge@samba.org>
2012-05-30 18:48:00 +1000

    save regions in joe.log

--- lib/cuav_joe.py
+++ lib/cuav_joe.py
@@ -11,35 +11,39 @@ import os, sys, cuav_util, mav_position, cPickle, time
 
 class JoePosition():
   '''a Joe position'''
-  def __init__(self, latlon, frame_time,
+  def __init__(self, latlon, frame_time, r,
                pos, image_filename):
       self.latlon = latlon
       self.frame_time = frame_time
       self.pos = pos
+      self.r = r
       self.image_filename = image_filename
 
   def __str__(self):
-    return 'JoePosition(lat=%f lon=%f %s %s %s)' % (self.latlon[0], self.latlon[1],
-                                                    self.pos, self.image_filename,
-                                                    time.asctime(time.localtime(self.frame_time)))
+    return 'JoePosition(lat=%f lon=%f %s %s %s %s)' % (self.latlon[0], self.latlon[1],
+                                                       self.pos, self.image_filename,
+                                                       str(self.r),
+                                                       time.asctime(time.localtime(self.frame_time)))
       
 class JoeLog():
-    '''a Joe position logger'''
-    def __init__(self, filename, append=True):
-        self.filename = filename
-        self.log = open(filename, "w+" if append else "w")
+  '''a Joe position logger'''
+  def __init__(self, filename, append=True):
+    self.filename = filename
+    self.log = open(filename, "w+" if append else "w")
         
-    def add(self, latlon, frame_time, pos, image_filename):
-        '''add an entry to the log'''
-        joe = JoePosition(latlon, frame_time, pos, image_filename)
-        self.log.write(cPickle.dumps(joe, protocol=cPickle.HIGHEST_PROTOCOL))
-        self.log.flush()
+  def add(self, latlon, frame_time, r, pos, image_filename):
+    '''add an entry to the log'''
+    joe = JoePosition(latlon, frame_time, r, pos, image_filename)
+    self.log.write(cPickle.dumps(joe, protocol=cPickle.HIGHEST_PROTOCOL))
+    self.log.flush()
         
-    def add_regions(self, frame_time, regions, pos, image_filename):
-        '''add a set of regions to the log, applying geo-referencing'''
-        for r in regions:
-            (lat, lon) = cuav_util.gps_position_from_image_region(r, pos)
-            self.add((lat,lon), frame_time, pos, image_filename)
+  def add_regions(self, frame_time, regions, pos, image_filename):
+    '''add a set of regions to the log, applying geo-referencing'''
+    for r in regions:
+      latlon = cuav_util.gps_position_from_image_region(r, pos)
+      if latlon is not None:
+        (lat, lon) = latlon
+        self.add((lat,lon), frame_time, r, pos, image_filename)
             
 class JoeIterator():
   '''an iterator for a joe.log'''
