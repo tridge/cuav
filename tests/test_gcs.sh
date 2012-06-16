ef4e8488712a5191e9858d58a70f7af0e404f78b
Andrew Tridgell <tridge@samba.org>
2012-06-16 14:29:37 +1000

    test: enable map interface

--- tests/test_gcs.sh
+++ tests/test_gcs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 rm -rf gtest
-mavproxy.py --aircraft=gtest --console --master 127.0.0.1:2626 --cmd='set moddebug 1; module load camera; camera boundary cuav/tests/CMAC-boundary.txt; camera view'
+mavproxy.py --aircraft=gtest --console --map --master 127.0.0.1:2626 --cmd='set moddebug 1; module load camera; camera boundary cuav/tests/CMAC-boundary.txt; camera view'
