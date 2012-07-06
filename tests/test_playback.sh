799840e5247180f64733c8824b229934b8513299
Andrew Tridgell <tridge@samba.org>
2012-07-06 16:40:14 +1000

    test_playback.sh script

---
+++ tests/test_playback.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+[ $# -eq 1 ] || {
+    echo "Usage: test_playback.sh <LOGDIR>"
+    exit 1
+}
+
+[ -r "$1"/flight.log ] || {
+    echo "Invalid log directory - $1/flight.log not found"
+    exit 1
+}
+[ -d "$1"/camera/raw ] || {
+    echo "Invalid log directory - $1/camera/raw not found"
+    exit 1
+}
+
+./cuav/tests/playback.py --loop --imagedir "$1"/camera/raw "$1"/flight.log
+
