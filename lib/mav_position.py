623c178003b136f2eb37fbf2aebbfd634c0f6658
Andrew Tridgell <tridge@samba.org>
2012-07-12 10:21:00 +1000

    mav_position: fixed interpolation and added gps_lag support

--- lib/mav_position.py
+++ lib/mav_position.py
@@ -36,7 +36,7 @@ class MavPosition():
 class MavInterpolator():
 	'''a class to interpolate position and attitude from a
 	series of mavlink messages'''
-	def __init__(self, backlog=100):
+	def __init__(self, backlog=500, gps_lag=0.5):
 		self.backlog = backlog
 		self.attitude = []
 		self.gps_raw = []
@@ -54,6 +54,9 @@ class MavInterpolator():
 		self.ground_pressure = None
 		self.ground_temperature = None
 		self.usec_base = 0
+		self.boot_offset = 0
+		self.last_msg_time = 0
+		self.gps_lag = gps_lag
 
 
 	def _find_msg_idx(self, type, t):
@@ -64,8 +67,12 @@ class MavInterpolator():
 		for i in range(len(a)-1, -1, -1):
 			if a[i]._timestamp <= t:
 				return i
-		raise MavInterpolatorException('no msgs of type %s before %s' % (
-			type, time.asctime(time.localtime(t))))
+		if len(a) > 0:
+			last_timestamp = time.asctime(time.localtime(a[-1]._timestamp))
+		else:
+			last_timestamp = ''
+		raise MavInterpolatorException('no msgs of type %s before %s last=%s' % (
+			type, time.asctime(time.localtime(t)), last_timestamp))
 
 	def _find_msg(self, type, t):
 		'''find the msg just before time t'''
@@ -78,7 +85,25 @@ class MavInterpolator():
 	def update_usec_base(self, msg):
 		'''update the difference between a usec field from
 		the APM and message timestamps'''
-		pass
+		usec = getattr(msg, 'time_usec', None)
+		if usec is None:
+			usec = getattr(msg, 'usec', None)
+		sec = usec*1.0e-6
+		offset = msg._timestamp - sec
+		if msg._timestamp > self.last_msg_time + 10:
+			# clock on PC has changed?
+			self.boot_offset = offset
+		self.last_msg_time = msg._timestamp
+		if self.boot_offset == 0:
+			self.boot_offset = offset
+		# check if time has wrapped
+		while offset > self.boot_offset + 4200:
+			self.boot_offset += (2**32)*1.0e-6
+			print("time wrapped: offset=%.2f boot_offset=%.2f sec=%.2f" % (offset, self.boot_offset, sec))
+		# assume minimum latency is most accurate
+		if offset < self.boot_offset:
+			self.boot_offset = offset
+			#print("link_lag=%f" % offset)
 				 
 	def add_msg(self, msg):
 		'''add in a mavlink message'''
@@ -100,7 +125,7 @@ class MavInterpolator():
 			'''keep self.backlog messages around of each type'''
 			while len(self.msg_map[type]) > self.backlog:
 				self.msg_map[type].pop(0)
-		if type == 'ATTITUDE':
+		if type == 'RAW_IMU':
 			self.update_usec_base(msg)
 
 	def _altitude(self, SCALED_PRESSURE):
@@ -123,7 +148,7 @@ class MavInterpolator():
 					raw = 'GPS_RAW_INT'
 				else:
 					raw = 'GPS_RAW'
-				gps_raw = self._find_msg('GPS_RAW', t)
+				gps_raw = self._find_msg(raw, t)
 				attitude = self._find_msg('ATTITUDE', t)
 				scaled_pressure = self._find_msg('SCALED_PRESSURE', t)
 				if (self.msg_map[raw][-1]._timestamp >= t and
@@ -136,6 +161,8 @@ class MavInterpolator():
 			if msg is None:
 				return MavInterpolatorException('end of logfile for timestamp %s' % time.asctime(time.localtime(t)))
 			self.add_msg(msg)
+			if msg._timestamp > t+3:
+				break
 
 	def interpolate(self, type, field, t, max_deltat=0):
 		'''find interpolated value for a field'''
@@ -150,6 +177,39 @@ class MavInterpolator():
 		if max_deltat != 0 and t2 - t1 > max_deltat:
 			raise MavInterpolatorDeltaTException('exceeded max_deltat %.1f' % (t2-t1))
 		return v1 + ((t-t1)/(t2-t1))*(v2-v1)
+
+	def interpolate_angle(self, type, field, t, max_deltat=0):
+		'''find interpolated value for a angle field in range -pi to pi'''
+		i = self._find_msg_idx(type, t)
+		a = self.msg_map[type]
+		if i == len(a)-1:
+			return getattr(a[i], field)
+		v1 = getattr(a[i], field)
+		v2 = getattr(a[i+1], field)
+		if abs(v1 - v2) > math.pi:
+			if v1 < v2:
+				v1 += 2*math.pi
+			else:
+				v2 += 2*math.pi
+		t1 = a[i]._timestamp
+		t2 = a[i+1]._timestamp
+		if max_deltat != 0 and t2 - t1 > max_deltat:
+			raise MavInterpolatorDeltaTException('exceeded max_deltat %.1f' % (t2-t1))
+		ret = v1 + ((t-t1)/(t2-t1))*(v2-v1)
+		if ret > math.pi:
+			ret -= 2*math.pi
+		return ret
+
+	def gps_time(self, gps_raw):
+		'''return a GPS packet timestamp in local seconds'''
+		usec = getattr(gps_raw, 'time_usec', None)
+		if usec is None:
+			usec = getattr(gps_raw, 'usec', None)
+		sec = usec*1.0e-6
+		if self.boot_offset == 0:
+			return gps_raw._timestamp
+		return self.boot_offset + sec		
+			
     
 	def position(self, t, max_deltat=0,roll=None):
 		'''return a MavPosition estimate given a time'''
@@ -158,25 +218,29 @@ class MavInterpolator():
 		scaled_pressure = self._find_msg('SCALED_PRESSURE', t)
 
 		# extrapolate our latitude/longitude 
+		gpst = t + self.gps_lag
 		if mavutil.mavlink10():
-			gps_raw = self._find_msg('GPS_RAW_INT', t)
+			gps_raw = self._find_msg('GPS_RAW_INT', gpst)
+			gps_timestamp = self.gps_time(gps_raw)
+			#print gps_raw._timestamp, gps_timestamp, gpst, t, gpst - gps_timestamp
 			(lat, lon) = cuav_util.gps_newpos(gps_raw.lat/1.0e7, gps_raw.lon/1.0e7,
-							  gps_raw.cog/100,
-							  (gps_raw.vel/100) * (t - gps_raw._timestamp))
+							  gps_raw.cog*0.01,
+							  (gps_raw.vel*0.01) * (gpst - gps_timestamp))
 		else:
-			gps_raw = self._find_msg('GPS_RAW', t)
+			gps_raw = self._find_msg('GPS_RAW', gpst)
+			gps_timestamp = self.gps_time(gps_raw)
 			(lat, lon) = cuav_util.gps_newpos(gps_raw.lat, gps_raw.lon,
 							  gps_raw.hdg,
-							  gps_raw.v * (t - gps_raw._timestamp))
+							  gps_raw.v * (gpst - gps_timestamp))
 
 		# get altitude
 		altitude = self._altitude(scaled_pressure)
 
 		# and attitude
 		if roll is None:
-			roll  = math.degrees(self.interpolate('ATTITUDE', 'roll', t, max_deltat))
-		pitch = math.degrees(self.interpolate('ATTITUDE', 'pitch', t, max_deltat))
-		yaw   = math.degrees(self.interpolate('ATTITUDE', 'yaw', t, max_deltat))
+			roll  = math.degrees(self.interpolate_angle('ATTITUDE', 'roll', t, max_deltat))
+		pitch = math.degrees(self.interpolate_angle('ATTITUDE', 'pitch', t, max_deltat))
+		yaw   = math.degrees(self.interpolate_angle('ATTITUDE', 'yaw', t, max_deltat))
 
 		return MavPosition(lat, lon, altitude, roll, pitch, yaw, t)
 	
