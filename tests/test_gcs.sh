ccf3cd7a1120578dac244f1acec76b57aa28329d
Andrew Tridgell <tridge@samba.org>
2012-07-20 16:52:31 +1000

    test: setup two links, to simulate real conditions

--- tests/test_gcs.sh
+++ tests/test_gcs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 rm -rf gtest
-mavproxy.py --aircraft=gtest --console --map --master 127.0.0.1:2626 --cmd='set moddebug 1; module load CUAV/camera; camera boundary cuav/tests/CMAC-boundary.txt; camera view'
+mavproxy.py --aircraft=gtest --console --map --master 127.0.0.1:2626 --master 127.0.0.1:2627 --cmd='set moddebug 1; module load CUAV/camera; camera boundary cuav/tests/CMAC-boundary.txt; camera view'
--- tests/test_plane.sh
+++ tests/test_plane.sh
@@ -2,4 +2,4 @@
 
 rm -rf ctest 
 export FAKE_CHAMELEON=1 
-MAVProxy/mavproxy.py --master 127.0.0.1:14550 --out 127.0.0.1:2626 --aircraft=ctest --cmd='set moddebug 1; module load CUAV/camera; camera gcs 127.0.0.1; camera boundary cuav/tests/CMAC-boundary.txt; camera save 0; camera start; watch camera'
+MAVProxy/mavproxy.py --master 127.0.0.1:14550 --out 127.0.0.1:2626 --out 127.0.0.1:2627 --aircraft=ctest --cmd='set moddebug 1; module load CUAV/camera; camera gcs 127.0.0.1; camera boundary cuav/tests/CMAC-boundary.txt; camera save 0; camera start; watch camera'
