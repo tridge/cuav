aa988c89267d4c6bf8ea055170a417974120f8a3
Andrew Tridgell <tridge@samba.org>
2012-09-20 19:48:22 +1000

    test_plane: fix pgm_save setting

--- tests/test_plane.sh
+++ tests/test_plane.sh
@@ -2,4 +2,4 @@
 
 rm -rf ctest 
 export FAKE_CHAMELEON=1 
-MAVProxy/mavproxy.py --master 127.0.0.1:14550 --out 127.0.0.1:2626 --out 127.0.0.1:2627 --aircraft=ctest --cmd='set moddebug 1; module load CUAV/camera; camera set gcs_address 127.0.0.1; camera boundary cuav/tests/CMAC-boundary.txt; camera set pgm_save 0; camera start; watch camera' "$@"
+MAVProxy/mavproxy.py --master 127.0.0.1:14550 --out 127.0.0.1:2626 --out 127.0.0.1:2627 --aircraft=ctest --cmd='set moddebug 1; module load CUAV/camera; camera set gcs_address 127.0.0.1; camera boundary cuav/tests/CMAC-boundary.txt; camera set save_pgm 0; camera start; watch camera' "$@"
