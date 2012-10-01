e74f1b4f03c30994a8404eaed0c4459d285efe99
Andrew Tridgell <tridge@samba.org>
2012-10-02 06:58:29 +1000

    adjust test scripts for kingaroy

--- tests/test_gcs.sh
+++ tests/test_gcs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 #rm -rf gtest
-mavproxy.py --aircraft=gtest --console --map --master 127.0.0.1:2626 --master 127.0.0.1:2627 --cmd='set moddebug 1; module load CUAV/camera; camera boundary cuav/tests/CMAC-boundary.txt; camera view' "$@"
+mavproxy.py --aircraft=gtest --console --map --master 127.0.0.1:2626 --master 127.0.0.1:2627 --cmd='set moddebug 1; module load CUAV/camera; camera boundary cuav/data/OBC_search2.txt; camera view' "$@"
 
