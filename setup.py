a704c0a67bc8e6851692961471ef01a7ac0e9840
Andrew Tridgell <tridge@samba.org>
2013-06-18 23:25:01 +1000

    up version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 import numpy as np
 import platform
 
-version = '1.0.1'
+version = '1.0.2'
 
 if platform.system() == 'Windows':
     jpegturbo_libpath = "c:\libjpeg-turbo-gcc\lib"
