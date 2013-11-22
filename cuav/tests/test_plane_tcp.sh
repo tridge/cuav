006e9292c3cc0bc8a9814e32e7e9bd6ee08fe94d
Andrew Tridgell <tridge@samba.org>
2013-11-22 14:18:19 +1100

    tests: updated tcp test

--- cuav/tests/test_plane_tcp.sh
+++ cuav/tests/test_plane_tcp.sh
@@ -2,4 +2,4 @@
 
 rm -rf ctest/log
 export FAKE_CHAMELEON=1 
-mavproxy.py --master tcp:127.0.0.1:5760 --out 127.0.0.1:14550 --out 127.0.0.1:2626 --out 127.0.0.1:2627 --aircraft=ctest --cmd='watch camera' "$@"
+mavproxy.py --master tcp:127.0.0.1:5760 --out 127.0.0.1:14550 --out 127.0.0.1:2626 --out 127.0.0.1:2627 --aircraft=ctest "$@"
