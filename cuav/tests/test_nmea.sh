bf70cd51cce9a15f7aa687275f6a1747be624d38
Andrew Tridgell <tridge@samba.org>
2013-11-23 22:31:16 +1100

    tests: fixed nmea module path

--- cuav/tests/test_nmea.sh
+++ cuav/tests/test_nmea.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 #rm -rf gtest
-mavproxy.py --aircraft=gtest --console --master 127.0.0.1:2626 --master 127.0.0.1:2627 --cmd='set moddebug 1; module load CUAV.nmea' "$@"
+mavproxy.py --aircraft=gtest --console --master 127.0.0.1:2626 --master 127.0.0.1:2627 --cmd='set moddebug 1; module load cuav.modules.nmea' "$@"
 
