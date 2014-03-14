a2fab5e9b8b8365b2043850a2ed0d37d31a52d63
Andrew Tridgell <tridge@samba.org>
2014-03-14 18:06:55 +1100

    updated example startup scripts

--- script/mav_beaver.sh
+++ script/mav_beaver.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
+# example startup script for cuav system on aircraft side
 
+AIRCRAFT=Beaver
 GCS=192.168.16.15
 
-ntpdate -b -v $GCS
+rdate $GCS
 date
 
-mavproxy.py --baudrate 57600 --master /dev/ttyUSB0 --out=$GCS:2626 --aircraft=Beaver "$@"
+mavproxy.py --baudrate 57600 --master /dev/ttyUSB0 --out=$GCS:2626 --aircraft=$AIRCRAFT "$@"
 
 
 
