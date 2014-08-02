b0b3f65d1d605248469a45bd464decc324e466e9
Andrew Tridgell <tridge@samba.org>
2014-08-02 18:15:04 +1000

    script: startup scripts for 2014 OBC

---
+++ script/mav_bushmaster.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rdate 192.168.16.15
+
+mavproxy.py --master /dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0 --baudrate 57600 --aircraft Bushmaster --out 192.168.16.15:2626
