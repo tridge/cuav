e58d4d72c1096782fe90dfddbe7cca3ff979162f
Andrew Tridgell <tridge@samba.org>
2014-09-08 11:38:43 +1000

    Bushmaster: update FTDI ports

--- script/Bushmaster.ground/mav_bushmaster.sh
+++ script/Bushmaster.ground/mav_bushmaster.sh
@@ -4,6 +4,9 @@
 PORT="/dev/serial/by-id/usb-FTDI_FT231X_USB_UART_DN0074S3-if00-port0"
 #PORT="/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A4*"
 
+# new RFD900
+PORT="/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A703V47G-if00-port0"
+
 #mavproxy.py --speech --map --aircraft=Porter --console --baudrate 57600 --master $PORT --out=127.0.0.1:14550 --out=127.0.0.1:14551 "$@"
 mavproxy.py --map --aircraft=Bushmaster --console --baudrate 57600 --master=$PORT --master 192.168.16.15:2626 --out=127.0.0.1:14550 $*
 pkill -f speech
