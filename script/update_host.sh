c385ec1e4a6f1498ff15bf647defd4ffc28b6248
Andrew Tridgell <tridge@samba.org>
2014-09-15 09:47:41 +1000

    added host update script

---
+++ script/update_host.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -x
+set -e
+host=$1
+
+pushd mavlink
+git push $host
+ssh $host "cd mavlink/pymavlink && git reset --hard && rm -rf build && MAVLINK_DIALECT=ardupilotmega python setup.py clean build install"
+popd
+
+pushd MAVProxy
+git push $host
+ssh $host "cd MAVProxy && git reset --hard && rm -rf build && python setup.py clean build install"
+popd
+
+pushd cuav
+git push $host
+ssh $host "cd cuav && git reset --hard && rm -rf build && python setup.py clean build install"
+popd
+
