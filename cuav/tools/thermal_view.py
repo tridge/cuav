e4c82480b2908942244408a284561c2ba27082f8
Andrew Tridgell <tridge@samba.org>
2014-10-29 09:55:56 +1100

    added thermal_view.py to installed tools

--- cuav/tools/thermal_view.py
+++ cuav/tools/thermal_view.py
@@ -18,10 +18,30 @@ if len(args) < 1:
     print("please supply an image file name")
     sys.exit(1)
 
+raw_image = None
+
+def show_mask(raw):
+    '''show mask and min/max'''
+    mask = 0
+    minv = 65536
+    maxv = 0
+    raw = raw.byteswap(False)
+    for x in range(640):
+        for y in range(480):
+            v = raw[y][x]
+            minv = min(v, minv)
+            maxv = max(v, maxv)
+            mask = mask | v
+    print("Min=%u max=%u mask=0x%04x" % (minv, maxv, mask))
+    
+
 def convert_image(filename, threshold, blue_threshold, green_threshold):
     '''convert a file'''
+    global raw_image
     pgm = cuav_util.PGM(filename)
     im_640 = numpy.zeros((480,640,3),dtype='uint8')
+    raw_image = pgm.array
+    show_mask(raw_image)
     scanner.thermal_convert(pgm.array, im_640, threshold, blue_threshold, green_threshold)
 
     color_img = cv.CreateImageHeader((640, 480), 8, 3)
@@ -31,7 +51,7 @@ def convert_image(filename, threshold, blue_threshold, green_threshold):
 view_image = mp_image.MPImage(title='ThermalView',
                               width=640,
                               height=480,
-                              mouse_events=False,
+                              mouse_events=True,
                               key_events=True,
                               can_zoom=True,
                               can_drag=True)
@@ -56,6 +76,20 @@ def settings_callback(setting):
     '''called on a changed setting'''
     global changed
     changed = True
+
+
+def show_value(x,y):
+    global range
+    if raw_image is None:
+        return
+    try:
+        v = raw_image[y][x]
+    except Exception:
+        return
+    v1 = v>>8
+    v2 = v&0xFF
+    v3 = v2<<8 | v1
+    print(x,y, v3)
 
 settings.set_callback(settings_callback)
 
@@ -95,4 +129,7 @@ while True:
                 elif event.returnkey == 'previousImage':
                     image_idx -= 1
                 changed = True
+            elif event.ClassName == 'wxMouseEvent':
+                (x,y) = (event.X, event.Y)
+                show_value(x,y)
     time.sleep(0.1)
