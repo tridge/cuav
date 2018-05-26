4bfb3212df2ca541fcc73a4d72ac3cb82de42ba2
Stephen Dade <stephen_dade@hotmail.com>
2018-05-26 11:38:26 +1000

    Commented out the now-unneeded Python compactness test

--- tests/lib/test_cuav_region.py
+++ tests/lib/test_cuav_region.py
@@ -13,14 +13,14 @@ from cuav.lib import mav_position
 
 def test_Region():
     newregion = cuav_region.Region(10, 10, 25, 23, (10, 10), scan_score=450, compactness=10)
-    
+
     assert newregion.tuple() == (10, 10, 25, 23)
     assert str(newregion) == "(10, 10, 25, 23) latlon=None score=None"
     assert newregion.center() == (17, 16)
-    
+
     img = cv2.imread(os.path.join(os.getcwd(), 'tests', 'testdata', 'test-8bit.png'))
     newregion.draw_rectangle(img)
-    
+
 def test_RegionsConvert():
     regions = []
     regions.append((200, 100, 204, 103, 200, np.array([[10, 15, 15, 10], [11, 13, 13, 9], [10, 12, 15, 11]], np.float32)))
@@ -28,12 +28,12 @@ def test_RegionsConvert():
     im_orig = cv2.imread(os.path.join(os.getcwd(), 'tests', 'testdata', 'test-8bit.png'))
     regionsout = cuav_region.RegionsConvert(regions, cuav_util.image_shape(im_orig), cuav_util.image_shape(im_orig), calculate_compactness=True)
     assert len(regionsout) == 2
-    
-def test_array_compactness():
-    subimage = np.array([[10, 15, 15, 10], [11, 13, 13, 9], [10, 12, 15, 11]], np.float32)
-    cpt = cuav_region.array_compactness(subimage)
-    assert cpt > 0 and cpt < 1000
-    
+
+#def test_array_compactness():
+#    subimage = np.array([[10, 15, 15, 10], [11, 13, 13, 9], [10, 12, 15, 11]], np.float32)
+#    cpt = cuav_region.array_compactness(subimage)
+#    assert cpt > 0 and cpt < 1000
+
 def test_image_whiteness():
     im_orig = cv2.imread(os.path.join(os.getcwd(), 'tests', 'testdata', 'test-8bit.png'))
     region = (1020, 658, 30, 28)
@@ -42,7 +42,7 @@ def test_image_whiteness():
     wht = cuav_region.image_whiteness(hsvsubimage)
     assert wht > 0
     assert wht < 1
-    
+
 def test_raw_hsv_score():
     im_orig = cv2.imread(os.path.join(os.getcwd(), 'tests', 'testdata', 'test-8bit.png'))
     region = (1020, 658, 30, 28)
@@ -56,25 +56,25 @@ def test_raw_hsv_score():
     assert score[4] > 0
     assert score[5] > 0
     assert score[6] > 0
-    
+
 def test_log_scaling():
     assert cuav_region.log_scaling(20, 2) == 5.991464547107982
     assert cuav_region.log_scaling(1, 20) == 20
     assert cuav_region.log_scaling(3, 5) == 5.493061443340549
-    
+
 def test_hsv_score():
     im_orig = cv2.imread(os.path.join(os.getcwd(), 'tests', 'testdata', 'test-8bit.png'))
     region = (1020, 658, 30, 28)
     subimage = SubImage(im_orig, region)
     hsvsubimage = cv2.cvtColor(subimage, cv2.COLOR_RGB2HSV)
     newregion = cuav_region.Region(1020, 658, 1050, 678, (30, 30), compactness=5.4, scan_score=20)
-    
+
     cuav_region.hsv_score(newregion, hsvsubimage, False)
     assert newregion.hsv_score > 0
     assert newregion.whiteness > 0
     assert newregion.compactness > 0
     assert newregion.score > 0
-    
+
 def test_score_region():
     im_orig = cv2.imread(os.path.join(os.getcwd(), 'tests', 'testdata', 'test-8bit.png'))
     newregion = cuav_region.Region(1020, 658, 1050, 678, (30, 30), compactness=5.4, scan_score=20)
@@ -83,7 +83,7 @@ def test_score_region():
     assert newregion.whiteness > 0
     assert newregion.compactness > 0
     assert newregion.score > 0
-    
+
 def test_filter_regions():
     im_orig = cv2.imread(os.path.join(os.getcwd(), 'tests', 'testdata', 'test-8bit.png'))
     regions = []
@@ -94,7 +94,7 @@ def test_filter_regions():
     assert ret[0].scan_score == 20
     assert ret[0].score > 4
     assert ret[0].compactness == 5.4
-    
+
 def test_filter_boundary():
     OBC_boundary = cuav_util.polygon_load(os.path.join(os.getcwd(), 'tests', 'testdata', 'OBC_boundary.txt'))
     regions = []
@@ -111,8 +111,8 @@ def test_filter_boundary():
     assert len(ret) == 2
     assert ret[0].score == 0
     assert ret[1].score == 32
-    
-    
+
+
 def test_filter_radius():
     regions = []
     regOne = cuav_region.Region(1020, 658, 1050, 678, (30, 30))
