0e959de49f99067e2c783a94220b0a1c424e1b0e
Stephen Dade <stephen_dade@hotmail.com>
2018-07-30 19:26:31 +1000

    Capture Scripts - cleanup and simplification of scripts

--- capturescripts/chameleon/start_chameleon_capture.sh
+++ capturescripts/chameleon/start_chameleon_capture.sh
@@ -12,8 +12,6 @@ DATETIME_DIR=$(date +"%Y%m%d_%H-%M-%S")
 mkdir -p ${CAPTURE_DIR}/${DATETIME_DIR}
 
 # start chameleon capture. Images stored in ${CAPTURE_DIR}/${DATETIME_DIR}
-screen -L -d -m -S image_capture -s /bin/bash ./py_capture.py --save=${CAPTURE_DIR}/${DATETIME_DIR}/
+python ./py_capture.py --save=${CAPTURE_DIR}/${DATETIME_DIR}/
 
-# start MAVProxy logging
-screen -L -d -m -S mavproxy -s /bin/bash ../mavlog.sh ${CAPTURE_DIR}/${DATETIME_DIR}
 
--- capturescripts/stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-set -x
-
-screen -S image_capture -X quit
-screen -S mavproxy -X quit
