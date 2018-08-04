d3a667cb8958f4d6cd7e809f436c579048709216
Stephen Dade <stephen_dade@hotmail.com>
2018-08-04 20:02:22 +1000

    Capture - Added source code for cuavraw

--- capturescripts/RasPi/README.txt
+++ capturescripts/RasPi/README.txt
@@ -1,68 +1,27 @@
 This module provides a live image stream from a Rasberry Pi camera (V2.1)
 
-It uses a custom capture software "cuavraw", with the source code available at 
-https://github.com/CanberraUAV/rpi_userland. This provides high-resolution timestamps to
+It uses a custom capture software "cuavraw", which is derived from the 
+"rpi_userland" code at https://github.com/raspberrypi/userland.
+
+This provides high-resolution timestamps (to within 100ms) to
 raw images, which are then compressed to jpeg files.
 
+-----------Build-----------
+Note this will only build on a Pi.
 
------------Install-----------
-The cuav git repo must be in the ~/ directory
+The libjpeg-turbo library is required.
 
-Also, the libjpeg-turbo library is required.
+Install it via the following command:
+sudo apt-get install libjpeg62-turbo-dev
 
-Install these via the following commands:
-cd ~/
-git clone https://github.com/CanberraUAV/cuav
-sudo apt-get install libjpeg62-turbo-dev screen
-
------------APM Config--------
-Typically, the Ras Pi would be connected to the TELEM2 port 
-on the APM. In this case, the following params must be set to 
-configure the TELEM2 port to output Mavlink (V2) telemetry at
-115200bps:
-SERIAL2_PROTOCOL = 2
-SERIAL2_BAUD = 115
+Then build:
+./cmake
+make
 
 -----------Running-----------
 Run start_rpi_capture.sh to start the capture
-Run ../stop.sh to stop all capture processes
-Run ../clear_images.sh to delete all captured images from the Pi
-
-Note a telemetry stream will be available over the Zerotier IP.
-Connect to it via "mavproxy.py --master=udpout:<Pi Zerotier IP>:14550"
 
 Images are stored in jpg format in the ~/images_captured/<start datetime> folder.
 
 A link to the latest image will be stored in ~/images_captured/current.jpg
-
-Run the "joeenter.py" file to enter in known Joe locations after an image collection flight.
-The coordinates will be stored in ~/images_captured/<start datetime>/joe.txt
-
----------Uploading-----------
-Ensure any 3G dongles are disconnected from the Pi before uploading image data, as the datasets are
-quite large. It's recommended to use a LAN connection for this.
-
-Before uploading for the first time, some libraries need to be installed:
-sudo apt-get install python3-pip screen
-pip3 install boto3 --user
-You may need to run the above command twice (it may crash the first time)
-
-Run the uploader via:
-./dataupload.py
-The software will ask for a confirmation before uploading. It's worth double-checking the folder it's
-going to upload has a name corresponding to the date/time of the last flight.
-
----------Downloading-----------
-
-Before uploading for the first time, some libraries need to be installed:
-pip3 install awscli --user
-
-The download utlity will synchonise your local images data with that on AWS. Thus any existing files
-will not be re-downloaded.
-
-Run it by:
-./datadownload.py <path>
-
-Where <path> is an existing folder where all the image data is locally stored
-
 
---
+++ capturescripts/RasPi/cuav_util.c
@@ -0,0 +1,454 @@
+/*
+  extract RPI raw10 image, producing a 16 bit pgm and 8 bit ppm
+  
+  With thanks to http://github.com/6by9/RPiTest
+ */
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <time.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdbool.h>
+#include <sys/wait.h>
+#include <signal.h>       
+#include <math.h>       
+
+#include <jpeglib.h>
+#include "cuav_util.h"
+#include <sys/time.h>
+
+#pragma GCC optimize("O3")
+
+// offset from end of the to BRCM marker
+#define BRCM_OFFSET 10270208
+#define DATA_OFFSET 0x8000
+
+// RPI image size
+#define IMG_WIDTH 3280
+#define IMG_HEIGHT 2464
+
+#define PACKED __attribute__((__packed__))
+
+struct PACKED rgb8 {
+    uint8_t r, g, b;
+};
+
+struct PACKED rgbf {
+    float r, g, b;
+};
+
+/*
+  16 bit bayer grid
+ */
+struct PACKED bayer_image {
+    uint16_t data[IMG_HEIGHT][IMG_WIDTH];
+};
+
+/*
+  RGB image, 8 bit
+ */
+struct PACKED rgb8_image {
+    struct rgb8 data[IMG_HEIGHT][IMG_WIDTH];
+};
+
+/*
+  RGB image, float
+ */
+struct PACKED rgbf_image {
+    struct rgbf data[IMG_HEIGHT][IMG_WIDTH];
+};
+
+/*
+  len shading scaling array
+ */
+struct lens_shading {
+    float scale[IMG_HEIGHT][IMG_WIDTH];
+};
+
+struct brcm_header {
+    char tag[4]; // BRCM
+    uint8_t pad[172];
+    uint8_t name[32];
+    uint16_t width;
+    uint16_t height;
+    uint16_t padding_right;
+    uint16_t padding_down;
+    uint32_t dummy[6];
+    uint16_t transform;
+    uint16_t format;
+    uint8_t bayer_order;
+    uint8_t bayer_format;
+};
+
+extern void swab(const void *from, void *to, ssize_t n);
+
+static void extract_raw10(const uint8_t *b, uint16_t width, uint16_t height, uint16_t raw_stride, struct bayer_image *bayer)
+{
+    uint8_t data[raw_stride];
+    uint16_t row, col;
+    
+    for (row=0; row<height; row++) {
+        uint16_t *raw = &bayer->data[row][0];
+        memcpy(data, b, raw_stride);
+        b += raw_stride;
+        uint8_t *dp = &data[0];
+        for (col=0; col<width; col+=4, dp+=5) {
+            // the top two bits are packed into byte 4 of each group
+            raw[col+0] = dp[0] << 2 | (dp[4]&3);
+            raw[col+1] = dp[1] << 2 | ((dp[4]>>2)&3);
+            raw[col+2] = dp[2] << 2 | ((dp[4]>>4)&3);
+            raw[col+3] = dp[3] << 2 | ((dp[4]>>6)&3);
+        }
+    }
+}
+
+
+static void save_pgm(const struct bayer_image *bayer, const char *fname)
+{
+    FILE *f = fopen(fname, "w");
+    if (f == NULL) {
+        perror(fname);
+        exit(1);
+    }
+    fprintf(f, "P5\n%u %u\n65535\n", IMG_WIDTH, IMG_HEIGHT);
+    uint16_t y;
+    for (y=0; y<IMG_HEIGHT; y++) {
+        uint16_t row[IMG_WIDTH];
+        swab(&bayer->data[y][0], &row[0], IMG_WIDTH*2);
+        if (fwrite(&row[0], IMG_WIDTH*2, 1, f) != 1) {
+            printf("write error\n");
+            exit(1);
+        }
+    }
+
+    fclose(f);
+}
+
+static void save_ppm(const struct rgb8_image *rgb, const char *fname)
+{
+    FILE *f = fopen(fname, "w");
+    if (f == NULL) {
+        perror(fname);
+        exit(1);
+    }
+    fprintf(f, "P6\n%u %u\n255\n", IMG_WIDTH, IMG_HEIGHT);
+    if (fwrite(&rgb->data[0][0], IMG_WIDTH*3, IMG_HEIGHT, f) != IMG_HEIGHT) {
+        printf("write error\n");
+        exit(1);
+    }
+
+    fclose(f);
+}
+
+static void debayer_BGGR_float(const struct bayer_image *bayer, struct rgbf_image *rgb)
+{
+    /*
+      layout in the input image is in blocks of 4 values. The top
+      left corner of the image looks like this
+      B G B G
+      G R G R
+      B G B G
+      G R G R
+    */
+    uint16_t x, y;
+    for (y=1; y<IMG_HEIGHT-2; y += 2) {
+        for (x=1; x<IMG_WIDTH-2; x += 2) {
+            rgb->data[y+0][x+0].r = bayer->data[y+0][x+0];
+            rgb->data[y+0][x+0].g = ((uint16_t)bayer->data[y-1][x+0] + (uint16_t)bayer->data[y+0][x-1] +
+                                     (uint16_t)bayer->data[y+1][x+0] + (uint16_t)bayer->data[y+0][x+1]) >> 2;
+            rgb->data[y+0][x+0].b = ((uint16_t)bayer->data[y-1][x-1] + (uint16_t)bayer->data[y+1][x-1] +
+                                     (uint16_t)bayer->data[y-1][x+1] + (uint16_t)bayer->data[y+1][x+1]) >> 2;
+            rgb->data[y+0][x+1].r = ((uint16_t)bayer->data[y+0][x+0] + (uint16_t)bayer->data[y+0][x+2]) >> 1;
+            rgb->data[y+0][x+1].g = bayer->data[y+0][x+1];
+            rgb->data[y+0][x+1].b = ((uint16_t)bayer->data[y-1][x+1] + (uint16_t)bayer->data[y+1][x+1]) >> 1;
+
+            rgb->data[y+1][x+0].r = ((uint16_t)bayer->data[y+0][x+0] + (uint16_t)bayer->data[y+2][x+0]) >> 1;
+            rgb->data[y+1][x+0].g = bayer->data[y+1][x+0];
+            rgb->data[y+1][x+0].b = ((uint16_t)bayer->data[y+1][x-1] + (uint16_t)bayer->data[y+1][x+1]) >> 1;
+
+            rgb->data[y+1][x+1].r = ((uint16_t)bayer->data[y+0][x+0] + (uint16_t)bayer->data[y+2][x+0] +
+                                     (uint16_t)bayer->data[y+0][x+2] + (uint16_t)bayer->data[y+2][x+2]) >> 2;
+            rgb->data[y+1][x+1].g = ((uint16_t)bayer->data[y+0][x+1] + (uint16_t)bayer->data[y+1][x+2] +
+                                     (uint16_t)bayer->data[y+2][x+1] + (uint16_t)bayer->data[y+1][x+0]) >> 2;
+            rgb->data[y+1][x+1].b = bayer->data[y+1][x+1];
+        }
+        rgb->data[y+0][0] = rgb->data[y+0][1];
+        rgb->data[y+1][0] = rgb->data[y+1][1];
+        rgb->data[y+0][IMG_WIDTH-1] = rgb->data[y+0][IMG_WIDTH-2];
+        rgb->data[y+1][IMG_WIDTH-1] = rgb->data[y+1][IMG_WIDTH-2];
+    }
+    memcpy(rgb->data[0], rgb->data[1], IMG_WIDTH*sizeof(rgb->data[0][0]));
+    memcpy(rgb->data[IMG_HEIGHT-1], rgb->data[IMG_HEIGHT-2], IMG_WIDTH*sizeof(rgb->data[0][0]));
+}
+
+static void rgbf_change_saturation(struct rgbf_image *rgbf, float change)
+{
+    const float Pr = .299;
+    const float Pg = .587;
+    const float Pb = .114;
+
+    uint16_t x, y;
+    for (y=0; y<IMG_HEIGHT; y++) {
+        for (x=0; x<IMG_WIDTH; x++) {
+            float r = rgbf->data[y][x].r;
+            float g = rgbf->data[y][x].g;
+            float b = rgbf->data[y][x].b;
+            //float P = sqrtf(r*r+Pr + g*g*Pg + b*b*Pb);
+            float P = Pr*r + Pb*b + Pg*g;
+            rgbf->data[y][x].r = P + (r - P) * change;
+            rgbf->data[y][x].g = P + (g - P) * change;
+            rgbf->data[y][x].b = P + (b - P) * change;
+        }
+    }
+}
+
+static struct lens_shading *shading;
+const float shading_scale_factor = 1.9;
+
+/*
+  create a lens shading correction array
+ */
+static void create_lens_shading(void)
+{
+    shading = malloc(sizeof(*shading));
+    uint16_t y, x;
+    for (y=0; y<IMG_HEIGHT; y++) {
+        for (x=0; x<IMG_WIDTH; x++) {
+            float dx = fabsf(((float)x) - IMG_WIDTH/2) / (IMG_WIDTH/2);
+            float dy = fabsf(((float)y) - IMG_HEIGHT/2) / (IMG_HEIGHT/2);
+            float from_center = sqrt(dx*dx + dy*dy);
+            if (from_center > 1.0) {
+                from_center = 1.0;
+            }
+            shading->scale[y][x] = 1.0 + from_center * shading_scale_factor;
+        }
+    }
+}
+
+static void rgbf_to_rgb8(const struct rgbf_image *rgbf, struct rgb8_image *rgb8)
+{
+    float highest = 0;
+    uint16_t x, y;
+    
+    for (y=0; y<IMG_HEIGHT; y++) {
+        for (x=0; x<IMG_WIDTH; x++) {
+            const struct rgbf *d = &rgbf->data[y][x];
+            if (d->r > highest) {
+                highest = d->r * shading->scale[y][x];
+            }
+            if (d->g > highest) {
+                highest = d->g * shading->scale[y][x];
+            }
+            if (d->b > highest) {
+                highest = d->b * shading->scale[y][x];
+            }
+        }
+    }
+
+    float scale = 255 / highest;
+    const float cscale[3] = { 1, 0.48, 0.82 };
+#define MIN(a,b) ((a)<(b)?(a):(b))
+    for (y=0; y<IMG_HEIGHT; y++) {
+        for (x=0; x<IMG_WIDTH; x++) {
+            float shade_scale = shading->scale[y][x];
+            if (rgbf->data[y][x].r >= 1022) {
+                rgb8->data[y][x].r = 255;
+            } else {
+                rgb8->data[y][x].r = MIN(rgbf->data[y][x].r * scale * cscale[0] * shade_scale, 255);
+            }
+            if (rgbf->data[y][x].g >= 1022) {
+                rgb8->data[y][x].g = 255;
+            } else {
+                rgb8->data[y][x].g = MIN(rgbf->data[y][x].g * scale * cscale[1] * shade_scale, 255);
+            }
+            if (rgbf->data[y][x].b >= 1022) {
+                rgb8->data[y][x].b = 255;
+            } else {
+                rgb8->data[y][x].b = MIN(rgbf->data[y][x].b * scale * cscale[2] * shade_scale, 255);
+            }
+        }
+    }
+}
+
+/*
+  extract bayer data from a RPi image
+ */
+static void extract_rpi_bayer(const uint8_t *buffer, uint32_t size, struct bayer_image *bayer)
+{
+    const uint8_t *b;
+    b = &buffer[size-BRCM_OFFSET];
+    struct brcm_header header;
+
+    memcpy(&header, b, sizeof(header));
+
+    if (strncmp(header.tag, "BRCM", 4) != 0) {
+        printf("bad header name - expected BRCM\n");
+        exit(1);
+    }
+
+    uint32_t raw_stride = ((((((header.width + header.padding_right)*5)+3)>>2) + 31)&(~31));
+    
+    printf("Image %ux%u format %u '%s' stride:%u bayer_order:%u\n",
+           header.width, header.height, header.format, header.name, raw_stride,
+           header.bayer_order);
+
+    if (header.width != IMG_WIDTH || header.height != IMG_HEIGHT) {
+        printf("Unexpected image size\n");
+        exit(1);
+    }
+
+    b = &buffer[size - (BRCM_OFFSET - DATA_OFFSET)];
+    
+    extract_raw10(b, header.width, header.height, raw_stride, bayer);
+}
+
+/*
+  write a JPG image
+ */
+static bool write_JPG(const char *filename, const struct rgb8_image *img, int quality)
+{
+    struct jpeg_compress_struct cinfo;
+    struct jpeg_error_mgr jerr;
+    FILE *outfile;
+    
+    cinfo.err = jpeg_std_error(&jerr);
+    jpeg_create_compress(&cinfo);
+
+    if ((outfile = fopen(filename, "wb")) == NULL) {
+        fprintf(stderr, "can't open %s\n", filename);
+        return false;
+    }
+    jpeg_stdio_dest(&cinfo, outfile);
+
+    cinfo.image_width = IMG_WIDTH;
+    cinfo.image_height = IMG_HEIGHT;
+    cinfo.input_components = 3;
+    cinfo.in_color_space = JCS_RGB;
+
+    jpeg_set_defaults(&cinfo);
+    jpeg_set_quality(&cinfo, quality, TRUE);
+
+    jpeg_start_compress(&cinfo, TRUE);
+
+    while (cinfo.next_scanline < cinfo.image_height) {
+        JSAMPROW row[1];
+        row[0] = (JSAMPROW)&img->data[cinfo.next_scanline][0];
+        jpeg_write_scanlines(&cinfo, row, 1);
+    }
+    
+    jpeg_finish_compress(&cinfo);
+    fclose(outfile);
+    jpeg_destroy_compress(&cinfo);
+
+    return true;    
+}
+
+static unsigned num_children_created;
+static volatile unsigned num_children_exited;
+
+static void child_exit(void)
+{
+    int status=0;
+    while (waitpid(-1, &status, WNOHANG) > 0) {
+        num_children_exited++;
+    }
+}
+
+/*
+  automatically cope with system load
+ */
+static void control_delay(void)
+{
+    static unsigned delay_us = 100000;
+    int children_active = (int)num_children_created - (int)num_children_exited;
+    if (children_active > 6) {
+        delay_us *= 1.2;
+    } else if (children_active < 4) {
+        delay_us *= 0.9;
+    }
+    printf("Delay %u active %d\n", delay_us, children_active);
+    usleep(delay_us);
+}
+
+
+void cuav_process(const uint8_t *buffer, uint32_t size, const char *filename, const char *linkname, const struct timeval *tv)
+{
+    printf("Processing %u bytes\n", size);
+    struct bayer_image *bayer;
+    struct rgbf_image *rgbf;
+    struct rgb8_image *rgb8;
+
+    struct tm tm;
+    time_t t = tv->tv_sec;
+    gmtime_r(&t, &tm);
+
+    char *fname = NULL;
+    asprintf(&fname, "%s%04u%02u%02u%02u%02u%02u%02uZ.jpg",
+             filename,
+             tm.tm_year+1900,
+             tm.tm_mon+1,
+             tm.tm_mday,
+             tm.tm_hour,
+             tm.tm_min,
+             tm.tm_sec,
+             tv->tv_usec/10000);
+    printf("fname=%s\n", fname);
+
+    char *fname_orig = NULL;
+    asprintf(&fname_orig, "%s%04u%02u%02u%02u%02u%02u%02uZ-orig.jpg",
+             filename,
+             tm.tm_year+1900,
+             tm.tm_mon+1,
+             tm.tm_mday,
+             tm.tm_hour,
+             tm.tm_min,
+             tm.tm_sec,
+             tv->tv_usec/10000);
+    
+    if (!shading) {
+        create_lens_shading();
+    }
+
+    if (num_children_created == 0) {
+        signal(SIGCHLD, child_exit);
+    }
+
+    num_children_created++;
+    if (fork() == 0) {
+        // run processing and saving in background
+
+        bayer = malloc(sizeof(*bayer));
+    
+        extract_rpi_bayer(buffer, size, bayer);
+
+        rgbf = malloc(sizeof(*rgbf));
+        debayer_BGGR_float(bayer, rgbf);
+        free(bayer);
+
+        rgbf_change_saturation(rgbf, 1.5);
+        
+        rgb8 = malloc(sizeof(*rgb8));
+        rgbf_to_rgb8(rgbf, rgb8);
+        free(rgbf);
+        
+		write_JPG(fname, rgb8, 100);
+		unlink(linkname);
+		symlink(fname, linkname);
+
+        free(rgb8);
+        signal(SIGCHLD, SIG_IGN);
+        _exit(0);
+    }
+
+    free(fname);
+    free(fname_orig);
+
+    control_delay();
+}
