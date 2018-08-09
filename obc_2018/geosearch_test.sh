9025cc0078c552aa673cb78ec686d6c1db8bb35b
Andrew Tridgell <andrew@tridgell.net>
2018-08-09 18:48:08 +1000

    obc: added cam params to test script

--- obc_2018/geosearch_test.sh
+++ obc_2018/geosearch_test.sh
@@ -7,4 +7,8 @@ FLAGS="--flag=-35.362669,149.164242,barrell"
 FLAGS="$FLAGS --flag=-35.362846,149.164272,barrell"
 FLAGS="$FLAGS --flag=-35.363027,149.164295,barrell"
 
-geosearch.py --minscore=500 $FLAGS $*
+# note that we need to use params.json, not params_half.json
+# for accurate georef
+CAM_PARMS="../cuav/data/PiCamV2/params.json"
+
+geosearch.py --camera-params=$CAM_PARMS --minscore=500 $FLAGS $*
