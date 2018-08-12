90e06078ce19715ead89748ef0538b17f1a1e5a9
Andrew Tridgell <andrew@tridgell.net>
2018-08-12 12:40:05 +1000

    added tridge ground for ranger

---
+++ script/2018/Ranger.Ground/StartTridge.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#The GCS just runs the cuav_ground module - image viewing
+
+#Note the GCS is receiving two streams - port 14650 for MAVLink telemetry
+#the image stream on port 14680. It will transmit remote commands back to the
+#cuav_air module on port 14670.
+
+#Type in "camera status" to see the queues of captured/processed/transmitted images
+
+#All telemetry from the Rpi on the Ranger
+mavproxy.py --master=udpin:10.26.0.200:14650 --mav20 --console --map --cmd="script StartTridge.scr"
