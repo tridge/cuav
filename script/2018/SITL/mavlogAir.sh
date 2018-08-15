63f59ff4b63b444029ccde81a9bbe10c87a77cda
Andrew Tridgell <andrew@tridgell.net>
2018-08-15 12:17:32 +1000

    SITL: use ozlabs proxy for SITL testing

--- script/2018/SITL/mavlogAir.sh
+++ script/2018/SITL/mavlogAir.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-#Just over Zerotier fro  the Pi on my desktop (assumes Ardupilot over USB)
-mavproxy.py --aircraft=Air --out=udpout:172.27.234.170:14650 --load-module=cuav.modules.camera_air --mav20 --cmd="script mavlogAir.scr"
+# go via ozlabs proxy
+OZLABS_PROXY1_AIR=udpout:203.11.71.1:10405
+OZLABS_PROXY1_GND=udpout:203.11.71.1:10406
 
+OZLABS_PROXY2_AIR=udpout:203.11.71.1:10407
+OZLABS_PROXY2_GND=udpout:203.11.71.1:10408
 
+# connect to local SITL instance, and out to proxy
+mavproxy.py --master :14550 --aircraft=Air --out=$OZLABS_PROXY1_AIR --load-module=cuav.modules.camera_air --mav20 --cmd="script mavlogAir.scr"
--- script/2018/SITL/mavlogGround.sh
+++ script/2018/SITL/mavlogGround.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# go via ozlabs proxy
+OZLABS_PROXY1_AIR=udpout:203.11.71.1:10405
+OZLABS_PROXY1_GND=udpout:203.11.71.1:10406
+
+OZLABS_PROXY2_AIR=udpout:203.11.71.1:10407
+OZLABS_PROXY2_GND=udpout:203.11.71.1:10408
+
 #Connects to mavlogAir
-mavproxy.py --master=udpin:172.27.234.170:14650 --mav20 --console --map --load-module=cuav.modules.camera_ground --cmd="script mavlogGround.scr"
+mavproxy.py --master=$OZLABS_PROXY1_GND --mav20 --console --map --load-module=cuav.modules.camera_ground --cmd="script mavlogGround.scr"
