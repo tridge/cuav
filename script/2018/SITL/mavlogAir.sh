865b1c3b303701528dd23954fc035e4bfead5806
Andrew Tridgell <andrew@tridgell.net>
2018-08-16 16:00:55 +1000

    SITL: setup for dual-link testing

--- script/2018/SITL/mavlogAir.sh
+++ script/2018/SITL/mavlogAir.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # go via ozlabs proxy
-OZLABS_PROXY1_AIR=udpout:203.11.71.1:10405
-OZLABS_PROXY1_GND=udpout:203.11.71.1:10406
+OZLABS_PROXY1_AIR=udpout:203.11.71.1:10401
+OZLABS_PROXY1_GND=udpout:203.11.71.1:10402
 
-OZLABS_PROXY2_AIR=udpout:203.11.71.1:10407
-OZLABS_PROXY2_GND=udpout:203.11.71.1:10408
+OZLABS_PROXY2_AIR=udpout:203.11.71.1:10403
+OZLABS_PROXY2_GND=udpout:203.11.71.1:10404
 
 # connect to local SITL instance, and out to proxy
 mavproxy.py --master :14550 --aircraft=Air --out=$OZLABS_PROXY1_AIR --load-module=cuav.modules.camera_air --mav20 --cmd="script mavlogAir.scr"
--- script/2018/SITL/mavlogGround.sh
+++ script/2018/SITL/mavlogGround.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # go via ozlabs proxy
-OZLABS_PROXY1_AIR=udpout:203.11.71.1:10405
-OZLABS_PROXY1_GND=udpout:203.11.71.1:10406
+OZLABS_PROXY1_AIR=udpout:203.11.71.1:10401
+OZLABS_PROXY1_GND=udpout:203.11.71.1:10402
 
-OZLABS_PROXY2_AIR=udpout:203.11.71.1:10407
-OZLABS_PROXY2_GND=udpout:203.11.71.1:10408
+OZLABS_PROXY2_AIR=udpout:203.11.71.1:10403
+OZLABS_PROXY2_GND=udpout:203.11.71.1:10404
 
 #Connects to mavlogAir
 mavproxy.py --master=$OZLABS_PROXY1_GND --mav20 --console --map --load-module=cuav.modules.camera_ground --cmd="script mavlogGround.scr"
