f8ff3a04dec865516ce5bc7da76cb0ce70b46569
Andrew Tridgell <andrew@tridgell.net>
2018-08-18 14:43:43 +1000

    lib: fixed indent

--- cuav/lib/block_xmit.py
+++ cuav/lib/block_xmit.py
@@ -459,10 +459,10 @@ class BlockSender:
 		if blk.callback:
                         #print("Callback %s" % blk.callback)
 			blk.callback()
-        if blk.sends == 0:
-            efficiency = blk.num_chunks / 1.0
-        else:
-            efficiency = blk.num_chunks / float(blk.sends)
+		if blk.sends == 0:
+			efficiency = blk.num_chunks / 1.0
+		else:
+			efficiency = blk.num_chunks / float(blk.sends)
 		#print("_complete_send: efficiency=%.2f sends=%u recvs=%u" % (efficiency, self.send_count, self.recv_count))
 		self.efficiency = 0.95 * self.efficiency + 0.05 * efficiency
 
