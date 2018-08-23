5ec9fdd5009e536fdd6b4100f2b75e1b17d03631
Andrew Tridgell <andrew@tridgell.net>
2018-08-23 18:24:40 +1000

    SITL: enable a tcp link for remotem commands

--- script/2018/SITL/mavlogGround.sh
+++ script/2018/SITL/mavlogGround.sh
@@ -8,4 +8,4 @@ OZLABS_PROXY2_AIR=udpout:203.11.71.1:10403
 OZLABS_PROXY2_GND=udpout:203.11.71.1:10404
 
 #Connects to mavlogAir
-mavproxy.py --master=$OZLABS_PROXY1_GND --mav20 --console --map --load-module=cuav.modules.camera_ground --cmd="script mavlogGround.scr"
+mavproxy.py --master tcp:127.0.0.1:5762 --master=$OZLABS_PROXY1_GND --mav20 --console --map --load-module=cuav.modules.camera_ground --cmd="script mavlogGround.scr"
