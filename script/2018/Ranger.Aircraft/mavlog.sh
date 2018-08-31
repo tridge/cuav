32174ddecfd1ef4e8074bdaf874382e1324207b5
Andrew Tridgell <andrew@tridgell.net>
2018-08-31 16:34:28 +1000

    Ranger: update scripts

--- script/2018/Ranger.Aircraft/mavlog.sh
+++ script/2018/Ranger.Aircraft/mavlog.sh
@@ -9,10 +9,17 @@ fi
 LOG_DIR=$1
 shift
 
-cp mavinit.scr ${LOG_DIR}/mavinit.scr
+OZLABS_PROXY1_AIR=udpout:203.11.71.1:10401
+OZLABS_PROXY1_GND=udpout:203.11.71.1:10402
+
+TRIDGELL_PROXY1_AIR=udpout:203.217.61.45:10401
+TRIDGELL_PROXY1_GND=udpout:203.217.61.45:10402
+
+cp -f mavinit.scr ${LOG_DIR}/mavinit.scr
 
 cd ${LOG_DIR}
-#All outputs to Stephen's and Tridge's laptop via Zerotier
-mavproxy.py --master=/dev/serial0 --baud=115200 --out=udpout:10.26.0.225:14650 --out=udpout:10.26.0.200:14650 --mav20 --cmd="script mavinit.scr"
+#All outputs via 2 network links
+mavproxy.py --master=/dev/serial0 --baud=115200 --out=$OZLABS_PROXY1_AIR --out=$TRIDGELL_PROXY1_AIR --mav20 --cmd="script mavinit.scr" $*
+
 
 
--- script/2018/Ranger.Aircraft/start_rpi_capture.sh
+++ script/2018/Ranger.Aircraft/start_rpi_capture.sh
@@ -6,16 +6,8 @@ set -x
 # set timezone to GMT
 export TZ=GMT
 
-CAPTURE_DIR=$HOME/images_captured/$(date +"%Y%m%d_%H-%M-%S")/
+export CAPTURE_DIR=$HOME/images_captured/$(date +"%Y%m%d_%H-%M-%S")/
 
 mkdir -p ${CAPTURE_DIR}
 
-#start main screen
-screen -AdmS mav_shell -t tab0 bash
-
-#and sub-screen tabs
-# start rpi capture. Images stored in ${CAPTURE_DIR}/${DATETIME_DIR}
-screen -S mav_shell -X screen -t image_capture ~/cuav/capturescripts/RasPi/cuavraw --halfres -o ${CAPTURE_DIR} -l ~/images_captured/capture.jpg
-# start MAVProxy logging
-screen -S mav_shell -X screen -t mavproxy ./mavlog.sh ${CAPTURE_DIR}
-
+screen -L -S mav_shell -d -m -c ./screenrc
