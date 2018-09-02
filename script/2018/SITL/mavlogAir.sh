69269f43fcb9249327dacad564d0b44b5a940c4c
Andrew Tridgell <andrew@tridgell.net>
2018-09-02 15:03:23 +1000

    SITL: setup for new network layout

--- script/2018/SITL/mavlogAir.sh
+++ script/2018/SITL/mavlogAir.sh
@@ -2,10 +2,7 @@
 
 # go via ozlabs proxy
 OZLABS_PROXY1_AIR=udpout:203.11.71.1:10401
-OZLABS_PROXY1_GND=udpout:203.11.71.1:10402
-
-OZLABS_PROXY2_AIR=udpout:203.11.71.1:10403
-OZLABS_PROXY2_GND=udpout:203.11.71.1:10404
+TRIDGELL_PROXY1_AIR=udpout:203.217.61.45:10401
 
 # connect to local SITL instance, and out to proxy
-mavproxy.py --master :14550 --aircraft=Air --out=$OZLABS_PROXY1_AIR --load-module=cuav.modules.camera_air --mav20 --cmd="script mavlogAir.scr"
+mavproxy.py --master tcp:127.0.0.1:5785 --aircraft=Air --out=$OZLABS_PROXY1_AIR --out=$TRIDGELL_PROXY1_AIR --load-module=cuav.modules.camera_air --mav20 --cmd="script mavlogAir.scr"
--- script/2018/SITL/mavlogGround.sh
+++ script/2018/SITL/mavlogGround.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# go via ozlabs proxy
-OZLABS_PROXY1_AIR=udpout:203.11.71.1:10401
 OZLABS_PROXY1_GND=udpout:203.11.71.1:10402
-
-OZLABS_PROXY2_AIR=udpout:203.11.71.1:10403
-OZLABS_PROXY2_GND=udpout:203.11.71.1:10404
+TRIDGELL_PROXY1_GND=udpout:203.217.61.45:10402
 
 #Connects to mavlogAir
-mavproxy.py --master tcp:127.0.0.1:5762 --master=$OZLABS_PROXY1_GND --mav20 --console --map --load-module=cuav.modules.camera_ground --cmd="script mavlogGround.scr"
+
+mavproxy.py --master=radio1 --master=$OZLABS_PROXY1_GND --master=$TRIDGELL_PROXY1_GND --mav20 --console --map --load-module=cuav.modules.camera_ground --cmd="script mavlogGround.scr"
