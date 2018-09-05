beb6648ee5ef4b2353d2e33d4728fe888e9e1ede
Stephen Dade <stephen_dade@hotmail.com>
2018-09-05 21:01:19 +1000

    Scripts - added Porter Backup GCS scripts

---
+++ script/2018/Porter.GroundBackup/StartGround.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#The GCS just runs the cuav_ground module - image viewing
+
+#Type in "camera status" to see the queues of captured/processed/transmitted images
+
+PORT=$(/bin/ls /dev/serial/by-id/usb-FTDI*)
+
+#All telemetry from the RFD900 or Tridge's laptop
+mavproxy.py --aircraft Porter --master $PORT --master :14660 --mav20 --console --map --cmd="script StartGround.scr" $*
+
