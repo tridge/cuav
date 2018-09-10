c6df0b31c2e6875ad69615e3c524548ef86b1b94
Peter Barker <pbarker@barker.dropbear.id.au>
2018-09-11 07:43:11 +1000

    tools: add original gegpsd-0.3.py

---
+++ cuav/tools/gegpsd/gegpsd-0.3.py
@@ -0,0 +1,50 @@
+#!env python
+import gps
+
+file = '/tmp/nmea.kml'
+
+session = gps.gps(host="localhost", port="2947")
+
+session.stream(flags=gps.WATCH_JSON)
+
+for report in session:
+   if report['class'] == 'TPV':
+       latitude  = report['lat']
+       longitude = report['lon']
+       altitude = report['alt']
+       speed_in = report['speed']
+       heading = report['track']
+
+       speed = int(speed_in * 1.852)
+       range = ( ( speed / 100  ) * 350 ) + 650
+       tilt = ( ( speed / 120 ) * 43 ) + 30
+
+       if speed < 10:
+           range = 200
+           tilt = 30
+           heading = 0
+
+       output = """<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://earth.google.com/kml/2.0">
+	<Placemark>
+		<name>%s km/h</name>
+		<description>^</description>
+		<LookAt>
+			<longitude>%s</longitude>
+			<latitude>%s</latitude>
+			<range>%s</range>
+			<tilt>%s</tilt>
+			<heading>%s</heading>
+		</LookAt>
+		<Point>
+			<coordinates>%s,%s,%s</coordinates>
+		</Point>
+	</Placemark>
+</kml>""" % (speed,longitude,latitude,range,tilt,heading,longitude,latitude,altitude)
+
+       f=open(file, 'w')
+       f.write(output)
+       f.close()
+
+del session
+
