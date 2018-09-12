e8b970a7451a451b68bbc0d5a9a36be7c86b73ec
Andrew Tridgell <andrew@tridgell.net>
2018-09-12 18:09:54 +1000

    SITL: improved raw script

--- script/2018/SITL/SIM_Kraken/start_kraken_raw.sh
+++ script/2018/SITL/SIM_Kraken/start_kraken_raw.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 OBC=$HOME/project/UAV/APM.obc2018
-$OBC/build/sitl/bin/arduplane --model plane -I 3 --uartA tcp:0 --uartC uart:../radio_relay --defaults $OBC/Tools/autotest/default_params/plane.parm
+
+pushd $OBC
+./waf configure --board sitl --debug
+./waf plane
+popd
+
+UARTC="uart:../radio_relay"
+
+$OBC/build/sitl/bin/arduplane --model plane -I 3 --uartA tcp:0 --uartC $UARTC --defaults $OBC/Tools/autotest/default_params/plane.parm
--- script/2018/SITL/SIM_Porter/start_quadplane_raw.sh
+++ script/2018/SITL/SIM_Porter/start_quadplane_raw.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 OBC=$HOME/project/UAV/APM.obc2018
-$OBC/build/sitl/bin/arduplane --model quadplane -I 2 --uartA tcp:0 --uartC uart:../radio_retrieval --defaults $OBC/Tools/autotest/default_params/quadplane.parm
+
+pushd $OBC
+./waf configure --board sitl --debug
+./waf plane
+popd
+
+UARTC="uart:../radio_retrieval"
+
+$OBC/build/sitl/bin/arduplane --model quadplane -I 2 --uartA tcp:0 --uartC $UARTC --defaults $OBC/Tools/autotest/default_params/quadplane.parm
