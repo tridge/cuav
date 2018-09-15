e8ace5488d4087abbcb68232111810e319974192
Andrew Tridgell <andrew@tridgell.net>
2018-09-15 14:08:04 +1000

    SIM_Porter: use quadplane-ice model

--- script/2018/SITL/SIM_Porter/start_quadplane_raw.sh
+++ script/2018/SITL/SIM_Porter/start_quadplane_raw.sh
@@ -8,4 +8,4 @@ popd
 
 UARTC="uart:../radio_retrieval"
 
-$OBC/build/sitl/bin/arduplane --model quadplane -I 2 --uartA tcp:0 --uartC $UARTC --defaults $OBC/Tools/autotest/default_params/quadplane.parm
+$OBC/build/sitl/bin/arduplane --model quadplane-ice -I 2 --uartA tcp:0 --uartC $UARTC --defaults $OBC/Tools/autotest/default_params/quadplane.parm
