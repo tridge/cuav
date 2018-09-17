a9eec83476f7593ac16665995c78c21fce247ed3
Andrew Tridgell <andrew@tridgell.net>
2018-09-17 15:51:31 +1000

    SITL: added vars for with radios

---
+++ script/2018/SITL/with_radio_vars.sh
@@ -0,0 +1,7 @@
+
+# dot this into env for running SITL tests with no radios attached
+
+export UART_RETRIEVAL="uart:../radio_retrieval"
+export UART_GROUND="radio1"
+export UART_RELAY="uart:../radio_relay"
+
