995c690732f6733527316b8f69bf74a98222b090
Stephen Dade <stephen_dade@hotmail.com>
2018-09-23 14:50:46 +1000

    Scriptsv - more GCS fixes for Stephen

--- script/2018/Porter.GroundBackup/StartGround.sh
+++ script/2018/Porter.GroundBackup/StartGround.sh
@@ -4,8 +4,8 @@
 
 #Type in "camera status" to see the queues of captured/processed/transmitted images
 
-PORT=$(/bin/ls /dev/serial/by-id/usb-FTDI*)
+PORT='radio'
 
 #All telemetry from the RFD900 or Tridge's laptop
-mavproxy.py --aircraft Porter --master $PORT --master :14660 --mav20 --console --map --force-connect --cmd="script StartGround.scr" $*
+mavproxy.py --aircraft Porter --master $PORT --force-connect --master :14660 --mav20 --console --map --cmd="script StartGround.scr" $*
 
