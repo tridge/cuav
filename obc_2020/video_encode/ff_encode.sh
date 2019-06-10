ca41fa6daa5cb4cbe9b2a7e8e450627eeee14255
Andrew Tridgell <andrew@tridgell.net>
2019-06-10 15:44:24 +1000

    use x265

--- obc_2020/video_encode/ff_encode.sh
+++ obc_2020/video_encode/ff_encode.sh
@@ -6,5 +6,5 @@ dirname=$1
     exit 1
 }
 
-ffmpeg -y -framerate 1 -i "$dirname"/'image%*.jpg' -c:v libx264 -vf fps=1 -t 360 -pix_fmt yuv420p "$dirname".mp4
+ffmpeg -y -framerate 1 -i "$dirname"/'image%*.jpg' -c:v libx265 -crf 37 -vf fps=1 -t 360 -pix_fmt yuv420p "$dirname".mp4
 ls -l "$dirname".mp4
